3c30f97c4fa37735d563fdf33330d87dc9661808
Alex Puello <alexpuello788@gmail.com>
2023-07-22 17:51:26 -1200

    change name

---
+++ prueba.py
@@ -0,0 +1,92 @@
+# print("\n\tBienvenido al extractor de codigo de area\n ")
+
+
+# import re 
+# text = input(str("Ingrese los numeros de telefono  "))
+
+# patron = re.compile(r'\(?(\d{3})\)?[-\s]?\d{3}[-\s]?\d{4}')
+# resultado = patron.findall(text)
+# print("\nLos codigos de area coincididos son :")
+# print(resultado)
+
+
+
+
+
+
+
+
+# Busca todas las ocurrencias de la exprecion regular en una cadena y los reenplaza con un texto especifico
+
+# import re 
+# cadena = "Hola juan como estas ayer te vi con jose "
+# pattern = r"juan"
+
+# nueva_cadena = re.sub(pattern, "alex", cadena)
+# print(nueva_cadena)
+
+
+
+
+# import re 
+# cadena = "Hola juan como estas este es mi numero de telefono 809-790-1234 "
+# pattern = r"12"
+
+# nueva_cadena = re.sub(pattern, "11", cadena)
+# print(nueva_cadena)
+
+
+
+
+
+# PRUEBAS DE LISTAS
+
+    # Cuenta el numero de veses que esta un character repetido en la lista 
+
+# list = ["a", "a", "a", "a", "e", "i", "o", "u"]
+# list = list.count("a")
+# print(list)
+
+
+
+
+    # Agrega un nuevo elemento al final de la lista 
+
+# list = ["a", "a", "a", "a", "e", "i", "o", "u"]
+# list.append("x")
+# print(list)
+
+
+
+
+    # Agrega un elemento en un lugar especifico de la lista
+
+# list = ["a", "a", "a", "a", "e", "i", "o", "u"]
+# list.insert(4,"g")
+# print(list)
+
+
+
+
+
+    # Remueve elementos especificos de una lista 
+
+# list = ["a", "a", "a", "a", "e", "i", "o", "u"]
+# list.remove("i")
+# list.remove("e")
+# list.remove("u")
+# print(list)
+
+
+
+#     Elimina todos los elementos de una lista 
+
+
+# list = ["a", "a", "a", "a", "e", "i", "o", "u"]
+# list.clear()
+# print(list)
+
+
+
+
+
